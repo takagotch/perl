fd40a0fd8df2f6436c32986c57352aeabb6b04d5
takagotch <30462722+takagotch@users.noreply.github.com>
2018-03-05 14:52:00 +0900

    Create fp.pl

---
+++ fp.pl
@@ -0,0 +1,8 @@
+print STDERR "This is a debugging msg.\n";
+
+print STDOUT "Please enter something: ";
+$response = <STDUB> // die "home come no input?";
+print STDOUT "Thank you!\n";
+
+while(<ARGV>){ ... }
+
